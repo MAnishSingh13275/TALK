5146ce41647aad84a0761fcbf1574bd077d03735
Manish Singh <manishsingh13275@gmail.com>
2022-08-25 19:25:49 +0530

    Solved socket.io bug

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
-const connectDB = require("./Config/db");
-const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
-const app = express();
 dotenv.config();
 connectDB();
+const app = express();
 
-app.use(express.json()); // To accept JSON data
+app.use(express.json()); // to accept json data
+
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// deployment
+// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -35,19 +37,29 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Error Handling
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-const server = app.listen(PORT, console.log(`Server ${PORT}`.yellow.bold));
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
+
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
-  cors: { origin: "http://localhost:3000" },
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
   socket.on("setup", (userData) => {
     socket.join(userData._id);
     socket.emit("connected");
@@ -55,8 +67,8 @@ io.on("connection", (socket) => {
 
   socket.on("join chat", (room) => {
     socket.join(room);
+    console.log("User Joined Room: " + room);
   });
-
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
